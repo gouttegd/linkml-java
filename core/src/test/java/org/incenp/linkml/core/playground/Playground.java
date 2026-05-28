aca205e0e482e73eea199da2d71ce8a6ea67f9c0
Damien Goutte-Gattat <damien@gerbi-gmb.de>
2026-05-28 11:48:46 +0100

    Add deserialisation tests for "refined inherited slots".
    Now that the runtime is able to detect the case of a refined inherited
    slot, add a test to confirm that it works as expected when deserialising
    data.

--- core/src/test/java/org/incenp/linkml/core/playground/Playground.java
+++ core/src/test/java/org/incenp/linkml/core/playground/Playground.java
@@ -1,11 +1,22 @@
 package org.incenp.linkml.core.playground;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Map;
+
+import org.incenp.linkml.core.ConverterContext;
 import org.incenp.linkml.core.LinkMLRuntimeException;
 import org.incenp.linkml.core.Slot;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+
 public class Playground {
+
+    private ConverterContext ctx = new ConverterContext();
 
     @Test
     void testFoo() {
@@ -132,5 +143,53 @@ public class Playground {
         barSlot = Slot.getSlot(ThirdDerivedFoo.class, "bars");
         Assertions.assertEquals(SecondDerivedBar.class, barSlot.getInnerType());
         Assertions.assertEquals(ThirdDerivedFoo.class, barSlot.getRefiningClass());
+    }
+
+    @Test
+    void testParsing() throws IOException, LinkMLRuntimeException {
+        Foo f = parse("refined-inherited-slots.yaml", Foo.class);
+        Assertions.assertInstanceOf(Bar.class, f.getBar());
+        Assertions.assertEquals("the bar", f.getBar().getName());
+        Assertions.assertInstanceOf(Bar.class, f.getBars().get(0));
+        Assertions.assertEquals("the first bar", f.getBars().get(0).getName());
+
+        FirstDerivedFoo fdf = parse("refined-inherited-slots.yaml", FirstDerivedFoo.class);
+        Assertions.assertInstanceOf(FirstDerivedBar.class, fdf.getBar());
+        Assertions.assertEquals("the bar", fdf.getBar().getName());
+        Assertions.assertEquals(1, fdf.getBar().getLength());
+        Assertions.assertInstanceOf(FirstDerivedBar.class, fdf.getBars().get(0));
+        Assertions.assertEquals("the first bar", fdf.getBars().get(0).getName());
+        Assertions.assertEquals(2, fdf.getBars().get(0).getLength());
+
+        SecondDerivedFoo sdf = parse("refined-inherited-slots.yaml", SecondDerivedFoo.class);
+        Assertions.assertInstanceOf(FirstDerivedBar.class, sdf.getBar());
+        Assertions.assertEquals("the bar", sdf.getBar().getName());
+        Assertions.assertEquals(1, sdf.getBar().getLength());
+        Assertions.assertInstanceOf(FirstDerivedBar.class, sdf.getBars().get(0));
+        Assertions.assertEquals("the first bar", sdf.getBars().get(0).getName());
+        Assertions.assertEquals(2, sdf.getBars().get(0).getLength());
+
+        ThirdDerivedFoo tdf = parse("refined-inherited-slots.yaml", ThirdDerivedFoo.class);
+        Assertions.assertInstanceOf(SecondDerivedBar.class, tdf.getBar());
+        Assertions.assertEquals("the bar", tdf.getBar().getName());
+        Assertions.assertEquals(1, tdf.getBar().getLength());
+        Assertions.assertInstanceOf(SecondDerivedBar.class, tdf.getBars().get(0));
+        Assertions.assertEquals("the first bar", tdf.getBars().get(0).getName());
+        Assertions.assertEquals(2, tdf.getBars().get(0).getLength());
+    }
+
+    private <T> T parse(String file, Class<T> target) throws IOException {
+        ObjectMapper mapper = new ObjectMapper(new YAMLFactory());
+        FileInputStream stream = new FileInputStream(new File("src/test/resources/core/samples/", file));
+        Object raw = mapper.readValue(stream, Map.class);
+
+        try {
+            Object cooked = ctx.getConverter(target).convert(raw, ctx);
+            Assertions.assertTrue(target.isInstance(cooked));
+            return target.cast(cooked);
+        } catch ( LinkMLRuntimeException e ) {
+            Assertions.fail("Unexpected exception", e);
+        }
+        return null;
     }
 }
