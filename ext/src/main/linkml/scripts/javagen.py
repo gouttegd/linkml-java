bd932f282c29ba1b9c921839af0f37eaa8e38793
Damien Goutte-Gattat <dgouttegattat@incenp.org>
2026-09-20 23:41:41 +0100

    Use upstream Java generator.
    The Java generator in LinkML has been updated to meet the requirements
    of the LinkML-Java runtime, we no longer need a custom generator and a
    custom template.
    
    We just keep a small wrapper script for convenience.

--- ext/src/main/linkml/scripts/javagen.py
+++ ext/src/main/linkml/scripts/javagen.py
@@ -2,84 +2,15 @@
 
 import click
 from pathlib import Path
-from linkml.generators.javagen import JavaGenerator
+from linkml.generators.javagen import cli
 
 
-def cleanup_dir(directory: Path) -> None:
-    if directory.exists():
-        for file in directory.iterdir():
-            file.unlink()
-        directory.rmdir()
-
-
-class CustomGenerator(JavaGenerator):
-
-    custom_type_map_read: bool = False
-    custom_type_map = {}
-
-    def lookup_custom_map_type(self, t):
-        if not self.custom_type_map_read:
-            mapfile = self.template_dir / "_types.map"
-            if mapfile.exists():
-                with mapfile.open("r") as f:
-                    for line in f:
-                        line = line.strip()
-                        if line.startswith("#"):
-                            continue
-                        items = line.split()
-                        if len(items) == 2:
-                            self.custom_type_map[items[0]] = items[1]
-            self.custom_type_map_read = True
-        return self.custom_type_map.get(t, None)
-
-    def map_type(self, t, required=False):
-        uri = self.get_type_uri(t.name)
-        if uri is not None:
-            custom_type = self.lookup_custom_map_type(uri)
-            if custom_type is not None:
-                return custom_type
-        return super().map_type(t, required)
-
-    def get_type_uri(self, name, custom_only=False):
-        if name in self.schemaview.all_types():
-            uri = self.schemaview.get_uri(name, expand=True, native=True)
-            if not uri.startswith("https://w3id.org/linkml/"):
-                uri = self.schemaview.get_uri(name, expand=True, native=False)
-            if not custom_only or self.lookup_custom_map_type(uri) is not None:
-                return uri
-        return None
-
-
-@click.option("--output-directory",
-              type=click.Path(dir_okay=True, file_okay=False, path_type=Path),
-              default=Path("ext/src/main/java"))
-@click.option("--schema-directory",
-              type=click.Path(dir_okay=True, file_okay=False, exists=True, path_type=Path),
-              default=Path("ext/src/main/linkml/schemas"))
-@click.option("--linkml-directory",
-              type=click.Path(dir_okay=True, file_okay=False, exists=True, path_type=Path),
-              default=Path("ext/src/main/resources/schemas/linkml"))
-@click.command()
-def cli(output_directory: Path, schema_directory: Path, linkml_directory: Path) -> None:
-
-    cleaned_up_dirs = {}
-
-    for schema in schema_directory.glob("**/*.yaml"):
-        package_dir = schema.relative_to(schema_directory).parent
-        output_dir = output_directory / package_dir
-        if output_dir not in cleaned_up_dirs:
-            cleanup_dir(output_dir)
-            cleaned_up_dirs[output_dir] = 1
-
-        package_name = package_dir.as_posix().replace("/", ".")
-        gen = CustomGenerator(schema,
-                              importmap={"linkml:": linkml_directory.absolute().as_posix()},
-                              true_enums=True,
-                              use_aliases=True,
-                              package=package_name,
-                              template_dir=Path("core/src/test/linkml/templates"))
-        gen.serialize(output_dir, template_variant="org.incenp.linkml")
-
-
-if __name__ == "__main__":
-    cli()
+linkml_directory = Path("ext/src/main/resources/schemas/linkml")
+ctx = click.Context(cli)
+ctx.invoke(cli,
+           yamlfile=Path("ext/src/main/linkml/schemas"),
+           output_directory=Path("ext/src/main/java"),
+           true_enums=True,
+           use_aliases=True,
+           template_variant="org.incenp.linkml",
+           importmap={"linkml:": linkml_directory.absolute().as_posix()})
